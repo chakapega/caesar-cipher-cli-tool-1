ed979b12256c52a9a464736e0378310e8d5ae806
melk0sha <din.ivanova13@gmail.com>
2020-03-26 03:16:08 +0400

    feat: add entry point with pipeline and main logics

---
+++ caesar-cipher-cli/index.js
@@ -0,0 +1,22 @@
+const { pipeline } = require("stream");
+const createReadStream = require("./streams/createReadStream");
+const createTransformStream = require("./streams/createTransformStream");
+const createWriteStream = require("./streams/createWriteStream");
+const createProgramOptions = require("./utils/createProgramOptions");
+
+const programOptions = createProgramOptions();
+
+const readStream = createReadStream(programOptions.input);
+const transformStream = createTransformStream(
+  programOptions.action,
+  programOptions.shift
+);
+const writeStream = createWriteStream(programOptions.output);
+
+pipeline(readStream, transformStream, writeStream, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Pipeline succeeded.");
+  }
+});
